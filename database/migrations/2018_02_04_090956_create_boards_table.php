74a1058da329db1dbabaaa6f42b71360ffe32437
XFeiF <eva.aeolus@gmail.com>
2018-02-04 17:20:40 +0800

    create tables

--- database/migrations/2018_02_04_090956_create_boards_table.php
+++ database/migrations/2018_02_04_090956_create_boards_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBoardsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('boards', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-
-            $table->rememberToken();
+            $table->longText('description')->nullable();
+            $table->integer('level')->unsigned()->assign(1);
+            $table->integer('user_id')->unsigned();
+            
+            $table->foreign('user_id')->references('id')->on('users');
+            
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('boards');
     }
 }
---
+++ database/migrations/2018_02_04_091018_create_sites_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('url', 255)->nullable();
+            $table->longText('description')->nullable();
+            $table->string('logo', 255)->nullable();
+            $table->integer('level')->unsigned()->assign(1);
+            $table->integer('card_id')->unsigned();
+            
+            $table->foreign('card_id')->references('id')->on('cards');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sites');
+    }
+}
