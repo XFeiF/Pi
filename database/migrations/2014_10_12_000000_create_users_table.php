8e9e109c9f7b824f0c365071aeb19afaaf91fbd7
XFeiF <eva.aeolus@gmail.com>
2018-02-04 16:50:49 +0800

    make auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('first_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('last_name')->nullable();
+            
             $table->rememberToken();
             $table->timestamps();
         });
